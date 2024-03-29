33161ec26a2929aef511f9f06fcc1cecfbfd8805
Scissord <mrnur@DESKTOP-TE7AEI5>
2024-03-29 21:51:36 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home, Library, About, NotFound } from '@pages';
 
 function App() {
-
+  // i need here layout
   return (
     <Router>
       <Routes>
